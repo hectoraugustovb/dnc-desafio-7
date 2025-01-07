a29117cbff86f322e41df2ce3ae827e9a5c4a672
hector59a2b9297 <hector.02220x@gmail.com>
2025-01-06 22:52:57 -0300

    Feat: Code converted

---
+++ server/src/module/aluno/aluno.route.ts
@@ -0,0 +1,18 @@
+import { Router, Request, Response, NextFunction } from 'express';
+import alunoModel from './aluno.model';
+
+const router = Router();
+
+router.get('/', (req: Request, res: Response, next: NextFunction) => {
+  alunoModel.getAll()
+    .then(data => res.status(200).json({ data }))
+    .catch(next);
+});
+
+router.post('/', async (req: Request, res: Response, next: NextFunction) => {
+  alunoModel.store(req.body)
+    .then(data => res.status(200).json({ data }))
+    .catch(next);
+});
+
+export default router;
